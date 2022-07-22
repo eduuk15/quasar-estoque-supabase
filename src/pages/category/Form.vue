75de77c490e43f199073e3e5a856ed49349a6832
Eduardo <eduardo@conectra.com.br>
2022-07-22 16:58:48 -0300

    feat: adiciona o metodo de edicao de categoria(tem erro pra resolver)

---
+++ src/pages/category/Form.vue
@@ -0,0 +1,100 @@
+<template>
+    <q-page padding>
+        <div class="row justify-center">
+            <div class="col-12 text-center">
+                <p class="text-h6">
+                    Form Category
+                </p>
+            </div>
+            <q-form class="col-md7 col-xs-12 col-sm-12 q-gutter-y-md" @submit.prevent="handleSubmit">
+                <q-input
+                    label="Name"
+                    v-model="form.name"
+                    lazy-rules
+                    :rules="[val => (val && val.length > 0) || 'Name is required']"
+                />
+
+                <q-btn
+                    :label="isUpdated ? 'Update' : 'Save'"
+                    color="primary"
+                    class="full-width"
+                    rounded
+                    type="submit"
+                />
+
+                <q-btn
+                    label="Cancel"
+                    color="primary"
+                    class="full-width"
+                    rounded
+                    flat
+                    :to="{ name: 'category' }"
+                />
+            </q-form>
+        </div>
+    </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted, computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import useApi from 'src/composables/UseApi'
+import useNotify from 'src/composables/UseNotify'
+
+export default defineComponent({
+  name: 'PageFormCategory',
+  setup () {
+    const table = 'category'
+    const router = useRouter()
+    const route = useRoute()
+    const { post, getById, update } = useApi()
+    const { notifyError, notifySuccess } = useNotify()
+
+    const isUpdated = computed(() => route.params.id)
+
+    let category = {}
+
+    const form = ref({
+      name: ''
+    })
+
+    onMounted(() => {
+      if (isUpdated.value) {
+        handleGetCategory(isUpdated.value)
+      }
+    })
+
+    const handleSubmit = async () => {
+      try {
+        if (isUpdated.value) {
+          await update(table, {
+            ...form
+          })
+          notifySuccess('Edited successfully!')
+        } else {
+          await post(table, form.value)
+          notifySuccess('Saved successfully!')
+        }
+        router.push({ name: 'category' })
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    const handleGetCategory = async (id) => {
+      try {
+        category = await getById(table, id)
+        form.value = category
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    return {
+      handleSubmit,
+      form,
+      isUpdated
+    }
+  }
+})
+</script>
