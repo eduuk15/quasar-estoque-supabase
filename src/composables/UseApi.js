0af574ec5e1d20ac8d592c937bb05a6b4c000b54
Eduardo <eduardo@conectra.com.br>
2022-07-22 15:00:44 -0300

    feat: adiciona a coluna das categorias no front, conectando com o supabase

--- src/composables/UseApi.js
+++ src/composables/UseApi.js
@@ -2,7 +2,7 @@ import useSupabase from 'src/boot/supabase'
 import useAuthUser from './UseAuthUser'
 
 export default function useApi () {
-  const supabase = useSupabase()
+  const { supabase } = useSupabase()
   const { user } = useAuthUser()
 
   const list = async (table) => {
--- src/pages/category/List.vue
+++ src/pages/category/List.vue
@@ -4,10 +4,11 @@
         <div class="row">
             <q-table
                 title="Category"
-                :rows="rows"
+                :rows="categories"
                 :columns="columns"
                 row-key="id"
                 class="col-12"
+                :loading="loading"
             >
             <template v-slot:top>
                 <span class="text-h6">
@@ -44,21 +45,39 @@ const columns = [
   { name: 'actions', align: 'right', label: 'Action', field: 'actions', sortable: true }
 ]
 
-const rows = [
-  {
-    id: '123',
-    name: 'Tênis'
-  }
-]
-
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import useApi from 'src/composables/UseApi'
+import useNotify from 'src/composables/UseNotify'
 
 export default defineComponent({
   name: 'PageCategoryList',
   setup () {
+    const categories = ref([])
+
+    const loading = ref(true)
+
+    const { list } = useApi()
+
+    const { notifyError } = useNotify()
+
+    const handleListCategories = async () => {
+      try {
+        loading.value = true
+        categories.value = await list('category')
+        loading.value = false
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    onMounted(() => {
+      handleListCategories()
+    })
+
     return {
       columns,
-      rows
+      categories,
+      loading
     }
   }
 })
