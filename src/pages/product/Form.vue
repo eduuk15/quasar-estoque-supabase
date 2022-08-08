50911908501ebbbee1b57bb77e149cd56ca2c182
Eduardo <eduardo@conectra.com.br>
2022-08-08 15:21:31 -0300

    feat: adiciona a lista e formulario de produtos

---
+++ src/pages/product/Form.vue
@@ -0,0 +1,140 @@
+<template>
+    <q-page padding>
+        <div class="row justify-center">
+            <div class="col-12 text-center">
+                <p class="text-h6">
+                    Form Product
+                </p>
+            </div>
+            <q-form class="col-md7 col-xs-12 col-sm-12 q-gutter-y-md" @submit.prevent="handleSubmit">
+                <q-input
+                    label="Name"
+                    v-model="form.name"
+                    lazy-rules
+                    :rules="[val => (val && val.length > 0) || 'Name is required']"
+                />
+
+                <q-editor
+                  v-model="form.description"
+                  min-height="5rem"
+                />
+
+                <q-input
+                    label="Amount"
+                    v-model="form.amount"
+                    lazy-rules
+                    :rules="[val => (val && val.length > 0) || 'Amount is required']"
+                    type="number"
+                />
+
+                <q-input
+                    label="Price"
+                    v-model="form.price"
+                    lazy-rules
+                    :rules="[val => (val && val.length > 0) || 'Price is required']"
+                    prefix="R$"
+                />
+
+                <q-select
+                  v-model="form.category_id"
+                  :options="optionsCategory"
+                  label="Category"
+                  option-value="id"
+                  option-label="name"
+                  map-options
+                  emit-value
+                />
+
+                <q-btn
+                    :label="isUpdated ? 'Update' : 'Save'"
+                    color="primary"
+                    class="full-width"
+                    rounded
+                    type="submit"
+                />
+
+                <q-btn
+                    label="Cancel"
+                    color="primary"
+                    class="full-width"
+                    rounded
+                    flat
+                    :to="{ name: 'product' }"
+                />
+            </q-form>
+        </div>
+    </q-page>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted, computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import useApi from 'src/composables/UseApi'
+import useNotify from 'src/composables/UseNotify'
+
+export default defineComponent({
+  name: 'PageFormCategory',
+  setup () {
+    const table = 'product'
+    const router = useRouter()
+    const route = useRoute()
+    const { post, getById, update, list } = useApi()
+    const { notifyError, notifySuccess } = useNotify()
+
+    const isUpdated = computed(() => route.params.id)
+
+    let product = {}
+    const optionsCategory = ref([])
+
+    const form = ref({
+      name: '',
+      description: '',
+      amount: 0,
+      price: 0,
+      category_id: ''
+    })
+
+    onMounted(() => {
+      handleListCategories()
+      if (isUpdated.value) {
+        handleGetProduct(isUpdated.value)
+      }
+    })
+
+    const handleListCategories = async () => {
+      optionsCategory.value = await list('category')
+    }
+
+    const handleSubmit = async () => {
+      try {
+        if (isUpdated.value) {
+          await update(table, form.value)
+          notifySuccess('Edited successfully!')
+        } else {
+          await post(table, form.value)
+          notifySuccess('Saved successfully!')
+        }
+        router.push({ name: 'product' })
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    const handleGetProduct = async (id) => {
+      try {
+        product = await getById(table, id)
+        form.value = product
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    return {
+      handleSubmit,
+      form,
+      isUpdated,
+      optionsCategory
+    }
+  }
+})
+</script>
---
+++ src/pages/product/List.vue
@@ -0,0 +1,136 @@
+
+<template>
+    <q-page padding>
+        <div class="row">
+            <q-table
+                title="Category"
+                :rows="products"
+                :columns="columnsProduct"
+                row-key="id"
+                class="col-12"
+                :loading="loading"
+            >
+            <template v-slot:top>
+                <span class="text-h6">
+                    Product
+                </span>
+                <q-space />
+                <q-btn
+                    v-if="$q.platform.is.desktop"
+                    label="Add New"
+                    color="primary"
+                    icon="mdi-plus"
+                    dense
+                    :to="{ name: 'form-product' }"
+                />
+            </template>
+              <template v-slot:body-cell-img_url="props">
+                <q-td :props="props">
+                  <q-avatar v-if="props.row.img_url">
+                    <img :src="props.row.img_url">
+                  </q-avatar>
+                  <q-avatar v-else color="red" text-color="white" icon="mdi-image-off" />
+                </q-td>
+              </template>
+                <template v-slot:body-cell-actions="props">
+                    <q-td :props="props" class="q-gutter-x-sm">
+                        <q-btn icon="mdi-pencil-outline" color="info" dense size="sm" @click="handleEdit(props.row)">
+                            <q-tooltip>
+                                Edit
+                            </q-tooltip>
+                        </q-btn>
+                        <q-btn icon="mdi-delete-outline" color="negative" dense size="sm" @click="handleRemoveProduct(props.row)">
+                             <q-tooltip>
+                                 Delete
+                            </q-tooltip>
+                        </q-btn>
+                    </q-td>
+                </template>
+            </q-table>
+        </div>
+        <q-page-sticky
+      position="bottom-right"
+      :offset="[18, 18]"
+      >
+        <q-btn
+          v-if="$q.platform.is.mobile"
+          fab
+          icon="mdi-plus"
+          color="primary"
+          :to="{ name: 'form-product' }"
+        />
+      </q-page-sticky>
+    </q-page>
+</template>
+
+<script>
+
+import { defineComponent, ref, onMounted } from 'vue'
+import useApi from 'src/composables/UseApi'
+import useNotify from 'src/composables/UseNotify'
+import { useRouter } from 'vue-router'
+import { useQuasar } from 'quasar'
+import { columnsProduct } from './table'
+
+export default defineComponent({
+  name: 'PageCategoryList',
+  setup () {
+    const products = ref([])
+
+    const loading = ref(true)
+
+    const router = useRouter()
+
+    const { list, remove } = useApi()
+
+    const { notifyError, notifySuccess } = useNotify()
+
+    const $q = useQuasar()
+
+    const table = 'product'
+
+    const handleListProducts = async () => {
+      try {
+        loading.value = true
+        products.value = await list(table)
+        loading.value = false
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    const handleEdit = (category) => {
+      router.push({ name: 'form-product', params: { id: category.id } })
+    }
+
+    const handleRemoveProduct = async (category) => {
+      try {
+        $q.dialog({
+          title: 'Confirm',
+          message: `Do you really want to delete ${category.name}?`,
+          cancel: true,
+          persistent: true
+        }).onOk(async () => {
+          await remove(table, category.id)
+          notifySuccess('Category deleted!')
+          handleListProducts()
+        })
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    onMounted(() => {
+      handleListProducts()
+    })
+
+    return {
+      columnsProduct,
+      products,
+      loading,
+      handleEdit,
+      handleRemoveProduct
+    }
+  }
+})
+</script>
