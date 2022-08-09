4f6fcbae71b36a3db0e7182bffb2e1766ba9f595
Eduardo <eduardo@conectra.com.br>
2022-08-09 16:38:51 -0300

    feat: detalhando produto e enviando pedido via whatsapp

---
+++ src/components/DialogProductDetails.vue
@@ -0,0 +1,74 @@
+<template>
+    <q-dialog
+        :full-width="$q.platform.is.mobile"
+        :model-value="show"
+        @before-hide="handleClose"
+    >
+        <q-card>
+            <q-card-section>
+                <div class="text-h6">
+                    Detalhes
+                </div>
+            </q-card-section>
+
+            <q-card-section>
+                <q-img :src="product.img_url" :ratio="4/3" style="min-width: 300px" />
+            </q-card-section>
+
+            <q-card-section>
+                <div class="text-h6">
+                    {{ product.name }}
+                </div>
+                <div class="text-subtitle2">
+                    {{ formatCurrency(product.price) }}
+                </div>
+                <div class="text-body2" v-html="product.description" />
+            </q-card-section>
+
+            <q-card-actions align="right">
+                <q-btn label="Cancel" color="primary" outline v-close-popup />
+                <q-btn
+                    label="Contact"
+                    icon="mdi-whatsapp"
+                    color="green-7"
+                    @click="handleSendWpp"
+                />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { formatCurrency } from 'src/utils/format'
+import { openURL } from 'quasar'
+
+export default defineComponent({
+  name: 'DialogProductDetails',
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    product: {
+      type: Object
+    }
+  },
+  setup (props, { emit }) {
+    const phone = '47996264041'
+    const msg = "Hello, I'm interested in the product: "
+    const handleClose = async () => {
+      emit('hideDialog')
+    }
+    const handleSendWpp = async () => {
+      const link = encodeURI(`https://api.whatsapp.com/send?phone=55${phone}&text=${msg} ${props.product.name} - ${formatCurrency(props.product.price)}`)
+      openURL(link)
+    }
+    return {
+      formatCurrency,
+      handleClose,
+      handleSendWpp
+    }
+  }
+})
+</script>
--- src/pages/product/Public.vue
+++ src/pages/product/Public.vue
@@ -25,7 +25,7 @@
 
           <template v-slot:item="props">
             <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
-              <q-card>
+              <q-card class="cursor-pointer" v-ripple:primary @click="handleShowDetails(props.row)">
                 <q-img :src="props.row.img_url" :ratio="4/3" />
                 <q-card-section class="text-center">
                   <div class="text-h6">{{ props.row.name }}</div>
@@ -36,6 +36,11 @@
           </template>
         </q-table>
       </div>
+      <dialog-product-details
+        :show="showDialogDetails"
+        :product="productDetails"
+        @hide-dialog="showDialogDetails = false"
+      />
     </q-page>
 </template>
 
@@ -46,9 +51,13 @@ import useNotify from 'src/composables/UseNotify'
 import { columnsProduct } from './table'
 import { useRoute } from 'vue-router'
 import { formatCurrency } from 'src/utils/format'
+import DialogProductDetails from 'components/DialogProductDetails.vue'
 
 export default defineComponent({
   name: 'PageProductPublic',
+  components: {
+    DialogProductDetails
+  },
   setup () {
     const products = ref([])
     const loading = ref(true)
@@ -57,6 +66,8 @@ export default defineComponent({
     const filter = ref('')
     const table = 'product'
     const route = useRoute()
+    const showDialogDetails = ref(false)
+    const productDetails = ref({})
 
     const handleListProducts = async (userId) => {
       try {
@@ -64,8 +75,13 @@ export default defineComponent({
         products.value = await listPublic(table, userId)
         loading.value = false
       } catch (error) {
-        notifyError('errou')
+        notifyError(error.message)
       }
+    }
+
+    const handleShowDetails = async (product) => {
+      productDetails.value = product
+      showDialogDetails.value = true
     }
 
     onMounted(() => {
@@ -79,7 +95,10 @@ export default defineComponent({
       products,
       loading,
       filter,
-      formatCurrency
+      formatCurrency,
+      showDialogDetails,
+      productDetails,
+      handleShowDetails
     }
   }
 })
